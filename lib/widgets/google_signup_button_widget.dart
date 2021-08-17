2a096cb34d18c18073b9d3fa4f8a58565095a351
MajesticRupesh <rupcomp1@gmail.com>
2021-08-17 19:50:22 +0530

    login screen change

--- lib/widgets/google_signup_button_widget.dart
+++ lib/widgets/google_signup_button_widget.dart
@@ -7,17 +7,16 @@ class GoogleSignupButtonWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Container(
         padding: EdgeInsets.all(4),
-        child: OutlineButton.icon(
+        child: FlatButton.icon(
           label: Text(
             'Sign In With Google',
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            style: TextStyle(fontSize: 20),
           ),
           shape: StadiumBorder(),
           padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-          highlightedBorderColor: Colors.black,
-          borderSide: BorderSide(color: Colors.black),
-          textColor: Colors.black,
-          icon: FaIcon(FontAwesomeIcons.google, color: Colors.red),
+          textColor: Colors.white,
+          icon: FaIcon(FontAwesomeIcons.google, color: Colors.white),
+          color: Color.fromRGBO(255, 23, 85, 1),
           onPressed: () {
             final provider = Provider.of<GoogleSignInProvider>(context, listen: false);
             provider.login();
--- lib/widgets/sign_up_widget.dart
+++ lib/widgets/sign_up_widget.dart
@@ -3,37 +3,50 @@ import 'google_signup_button_widget.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class SignUpWidget extends StatelessWidget {
-  final Widget svg = SvgPicture.asset('assets/loginasset.svg');
+  final Widget svg = SvgPicture.asset(
+    'assets/loginasset.svg',
+    fit: BoxFit.fitWidth,
+  );
   @override
-  Widget build(BuildContext context) => Stack(
-        fit: StackFit.expand,
-        children: [
-          svg,
-          buildSignUp(),
-        ],
-      );
-
-  Widget buildSignUp() => Column(
-        children: [
-          Spacer(),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Container(
-              margin: EdgeInsets.symmetric(horizontal: 20),
-              width: 175,
-              child: Text(
-                'Welcome to Flix',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
+  Widget build(BuildContext context) => Scaffold(
+        backgroundColor: Color.fromRGBO(30, 32, 70, 1),
+        body: Column(
+          children: [
+            svg,
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Container(
+                margin: EdgeInsets.only(top: 50),
+                child: Center(
+                  child: Text(
+                    'Welcome to Flix',
+                    style: TextStyle(
+                      color: Color.fromRGBO(240, 240, 240, 1),
+                      fontSize: 36,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                 ),
               ),
             ),
-          ),
-          Spacer(),
-          GoogleSignupButtonWidget(),
-          Spacer(),
-        ],
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Container(
+                margin: EdgeInsets.fromLTRB(20, 30, 20, 0),
+                child: Text(
+                  'Your very own movie list. Add/Delete/Edit your list in a jiffy with Flex.',
+                  style: TextStyle(
+                    color: Color.fromRGBO(200, 200, 200, 1),
+                    fontSize: 20,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ),
+            ),
+            Spacer(),
+            GoogleSignupButtonWidget(),
+            Spacer(),
+          ],
+        ),
       );
 }
